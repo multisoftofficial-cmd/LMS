52bf426a22c2464f1a8163992107ef605667976d
Multisoft <multisoftofficial@gmail.com>
2026-05-18 10:11:48 -0400

    model: Add SiteSetting model

---
+++ app/Models/SiteSetting.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiteSetting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'description',
+    ];
+
+    public static function get($key, $default = null)
+    {
+        $setting = static::where('key', $key)->first();
+        return $setting ? $setting->value : $default;
+    }
+
+    public static function set($key, $value, $type = 'string', $description = null)
+    {
+        return static::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value, 'type' => $type, 'description' => $description]
+        );
+    }
+}
